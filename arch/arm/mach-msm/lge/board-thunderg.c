f29055cd4162a84df003d6c5774112abe4d85ca2
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2013-01-29 20:38:49 +0100

    enable AXI OC

--- arch/arm/mach-msm/lge/board-thunderg.c
+++ arch/arm/mach-msm/lge/board-thunderg.c
@@ -447,7 +447,7 @@ static struct msm_acpu_clock_platform_data msm7x2x_clock_data = {
 	.acpu_switch_time_us = 50,
 	.max_speed_delta_khz = 400000,
 	.vdd_switch_time_us = 62,
-	.max_axi_khz = 160000,
+	.max_axi_khz = 200000
 };
 
 void msm_serial_debug_init(unsigned int base, int irq,
