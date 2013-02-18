0e3cb31e820a0f2c4c1e4d554428c29064f17ec7
Austin Au-Yeung <austinay1996@hotmail.com>
2013-02-17 21:26:37 -0800

    SmartassV2 is back

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -366,6 +366,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactivex)
