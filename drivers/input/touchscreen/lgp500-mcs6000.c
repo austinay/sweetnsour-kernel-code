69de05fffc3d2e58f845f29f0aa8eeaa59a50cf5
Austin Au-Yeung <austinay1996@hotmail.com>
2013-02-27 01:27:13 -0800

    fix compilation of DerCaveman TS driver

--- drivers/input/touchscreen/lgp500-mcs6000.c
+++ drivers/input/touchscreen/lgp500-mcs6000.c
@@ -694,7 +694,7 @@ static int mcs6000_release(struct inode *inode, struct file *file)
 
 static struct file_operations mcs6000_ts_ioctl_fops = {
 	.owner   = THIS_MODULE,
-	.ioctl   = mcs6000_ioctl,
+	.unlocked_ioctl   = mcs6000_ioctl,
 	.open    = mcs6000_open,
 	.release = mcs6000_release,
 };
